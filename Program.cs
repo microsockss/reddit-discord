62ef6cd80093b38c7ebd369a3d1a4baa2bb76887
Microsocks <barclayethan@gmail.com>
2018-12-30 18:10:22 -0500

    Bot is now functional

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ namespace DiscordBot
 {
     class Program
     {
+        public static int totalUsers = 0;
+        public const int ARRAY_SIZE = 1000;
+        public static int index = 0;
+        public static User[] user = new User[ARRAY_SIZE];
+
         static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
         private SocketGuild _guild;
         private DiscordSocketClient _client;
@@ -30,7 +35,10 @@ namespace DiscordBot
 
         public async Task RunBotAsync()
         {
-
+            //Initialize2();
+            //await Save();
+            //await Console.Out.WriteLineAsync("Created blank dataset.");
+            await Load();
             DiscordSocketConfig _config = new DiscordSocketConfig();
             _config.MessageCacheSize = 1;
 
@@ -66,7 +74,7 @@ namespace DiscordBot
             Console.WriteLine(arg);
 
             return Task.CompletedTask;
-        }
+        }   
 
         public async Task RegisterCommandsAsync()
         {
@@ -93,6 +101,8 @@ namespace DiscordBot
         {
             _client.ReactionAdded += HandleVoteAsync;
 
+            _client.ReactionRemoved += HandleVoteAsync;
+
             // await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
         }
 
@@ -100,11 +110,63 @@ namespace DiscordBot
         {
             var reaction = arg3.Emote;
 
-            var user = arg3.UserId;
+            var message = await arg1.DownloadAsync();
 
-            await Console.Out.WriteLineAsync("User ID: " + user);
+            SocketGuild guild = ((SocketGuildChannel)message.Channel).Guild;
 
-            await Console.Out.WriteLineAsync("Reaction ID: " + reaction);
+            IEmote upvote = guild.Emotes.First(e => e.Name == "upvote");
+            IEmote downvote = guild.Emotes.First(e => e.Name == "downvote");
+
+            var upvotes = message.Reactions[upvote].ReactionCount;
+            var downvotes = message.Reactions[downvote].ReactionCount;
+
+            var author = message.Author.Id.ToString();
+            
+            var voter = arg3.UserId;
+
+            await Console.Out.WriteLineAsync("Author ID: " + author);
+
+            await Console.Out.WriteLineAsync("Voter ID: " + voter);
+
+            await Console.Out.WriteLineAsync("Total Upvotes: " + upvotes);
+
+            await Console.Out.WriteLineAsync("Total Downvotes: " + downvotes);
+
+            int karma = upvotes - downvotes;
+
+            if (karma == 0) return;
+
+            // Search for user
+            for (int i = 1; i < ARRAY_SIZE; i++)
+            {
+                string tempUserId = user[i].UserId;
+                if (tempUserId == author)
+                {
+                    await Console.Out.WriteLineAsync("User found");
+                    index = i;
+                    break;
+                }
+                else if (i == ARRAY_SIZE - 1)
+                {
+                    if (totalUsers != 0 || totalUsers != 1)
+                    {
+                        totalUsers++;
+                    }
+                    user[totalUsers].UserId = author;
+                    index = 1;
+                    await Console.Out.WriteLineAsync("User not found, user " + author + " created as user " + totalUsers);
+                    if (totalUsers == 0)
+                    {
+                        totalUsers = 1;
+                    }
+                    break;
+                }
+            }
+
+            user[index].Karma += karma;
+
+            await Save();
+            Console.WriteLine("Granted karma and saved to database.");
 
             throw new NotImplementedException();
         }
@@ -112,17 +174,23 @@ namespace DiscordBot
         private async Task HandleCommandAsync(SocketMessage arg)
         {
             var message = arg as SocketUserMessage;
-
             if (message == null || message.Author.IsBot) return;
                                 
             int argPos = 0;
 
-            if (message.HasStringPrefix(".", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            if (message.HasStringPrefix("?", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
             {
                 var context = new SocketCommandContext(_client, message);
 
                 var result = await _commands.ExecuteAsync(context, argPos, _services);
 
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+
+
+            if (message.HasStringPrefix(".", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
                 SocketGuild guild = ((SocketGuildChannel)message.Channel).Guild;
 
                 IEmote upvote = guild.Emotes.First(e => e.Name == "upvote");
@@ -132,9 +200,38 @@ namespace DiscordBot
                 IEmote downvote = guild.Emotes.First(e => e.Name == "downvote");
                 await message.AddReactionAsync(downvote);
                 await Console.Out.WriteLineAsync("bot downvoted");
+            }
+        }
 
-                if (!result.IsSuccess)
-                    Console.WriteLine(result.ErrorReason);
+        public async Task Save()
+        {
+            List<User> users = new List<User>();
+            for (int i = 0; i < user.Length; i++)
+            {
+                users.Add(user[i]);
+            }
+
+            Serialize.WriteToBinaryFile("users.bin", users, false);
+            Serialize.WriteToBinaryFile("totalusers.bin", totalUsers, false);
+        }
+
+        public async Task Load()
+        {
+            List<User> users = new List<User>();
+            users = Serialize.ReadFromBinaryFile<List<User>>("users.bin");
+            totalUsers = Serialize.ReadFromBinaryFile<int>("totalusers.bin");
+
+            for (int i = 0; i < users.Count; i++)
+            {
+                user[i] = users[i];
+            }
+        }
+
+        public static void Initialize2()
+        {
+            for (int i = 0; i < user.Length; i++)
+            {
+                user[i] = new User();
             }
         }
     }
